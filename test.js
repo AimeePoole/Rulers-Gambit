c50ac69cbf03539ed5bf0a8ce45be9a8337b2b08
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-12 20:49:19 +0100

    Javascript fetch function for the test get request

---
+++ test.js
@@ -0,0 +1,7 @@
+fetch('http://127.0.0.1:8000/scenario')
+    .then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log('GET response text:');
+        console.log(text); // Print the greeting as text
+    });
