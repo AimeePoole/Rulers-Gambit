f50c800bb73988a565828dd9a1cad6bffebec417
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-13 12:46:16 +0100

    post test
    post test to see it the post code works before attempting to get the user to input data

--- appTest.py
+++ appTest.py
@@ -23,7 +23,6 @@ def scenario():
 
 @app.route('/scenario', methods=['GET'])
 @cross_origin(origins = jsOrigin)
-
 def find_all():
     db = sqlite3.connect('databaseTest.db')
     #a cursor lets you Send SQL queries to the database (e.g., SELECT, INSERT, UPDATE, DELETE) and fetch results from those queries
@@ -41,6 +40,41 @@ def find_all():
     'LEFT JOIN options ON options.scenario_id = scenario.id;')
     data = with_labels(cursor.fetchall(), ("id", "scenarioDescription","catagoryName","phaseName","option_id","optionDescription"))    
     return jsonify(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#post test
+@app.route('/scenarioPost', methods=['POST'])
+@cross_origin(origins = jsOrigin)
+def rate():
+    db = sqlite3.connect('databaseTest.db' , timeout=30)
+    cursor = db.cursor()
+
+ 
+    cursor.execute('INSERT OR IGNORE INTO scenario\
+    (id, scenarioDescription, catagory_id, phase_id) ' \
+    'VALUES ("s3", "A heatwave has swept the nation, resulting in civil unrest and exstresive damage to crops. You must find a way to rectifi the sitchuation will you?", "c1","p1")')
+
+
+    db.commit()
+    data = with_labels(cursor.fetchall(), ("id", "scenarioDescription","catagoryName","phaseName"))    
+    return jsonify(data)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8000, debug=True)
