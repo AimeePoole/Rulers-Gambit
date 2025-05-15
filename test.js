452552b3780ace1d10f2c5458d3258da44f86363
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-15 19:12:22 +0100

    console.log('GET response text:'); removed as test works

--- test.js
+++ test.js
@@ -14,7 +14,6 @@ fetch('http://127.0.0.1:8000/scenarioDetails')
     .then(function (response) {
         return response.text();
     }).then(function (text) {
-        console.log('GET response text:');
         console.log(text); 
     });
 
