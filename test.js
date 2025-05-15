2337dc0e9876516292b684f3c2c563f7360a39ae
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-15 13:39:17 +0100

    a new fetch request for scenarioDetails

--- test.js
+++ test.js
@@ -3,5 +3,15 @@ fetch('http://127.0.0.1:8000/scenario')
         return response.text();
     }).then(function (text) {
         console.log('GET response text:');
-        console.log(text); // Print the greeting as text
+        console.log(text);
     });
+
+
+fetch('http://127.0.0.1:8000/scenarioDetails')
+    .then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log('GET response text:');
+        console.log(text); 
+    });
+
