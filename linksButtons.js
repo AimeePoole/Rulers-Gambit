652815b08b9aab6fd07735c7378f1e9ea320ff2e
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-21 16:18:54 +0100

    a javascript file added that links to all the pages

---
+++ linksButtons.js
@@ -0,0 +1,39 @@
+//used indtead of links in html so that they can look like buttons
+function animationPageLink() {
+    window.location.href = "animation_page.html";
+}
+
+function endPageLink() {
+    window.location.href = "end_page.html";
+}
+
+function homePageLink() {
+    window.location.href = "Home_Page.html";
+}
+
+function lorePageLink() {
+    window.location.href = "Lore_page.html";
+}
+
+function rulesPageLink() {
+    window.location.href = "Rules.html";
+}
+
+function scorePageLink() {
+    window.location.href = "score_page.html";
+}
+
+function scenarioPageLink() {
+    window.location.href = "senario_page.html";
+}
+
+function statsPageLink() {
+    window.location.href = "stats_page.html";
+}
+
+function underConstructionPageLink() {
+    alert("The page you are trying to reach is under comstruction");
+}
+
+
+
