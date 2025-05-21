c1d35cb3e98476cebc499f79b1c797365462bfd1
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-21 23:57:42 +0100

    comments changed

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 function checkSum() {
+  //this gets all the stat values entered as consts 
     const Economy = parseInt(document.getElementById('economy').value);
     const Military = parseInt(document.getElementById('military').value);
     const Security = parseInt(document.getElementById('security').value);
@@ -6,17 +7,22 @@ function checkSum() {
     const Education = parseInt(document.getElementById('education').value);
     const Agriculture = parseInt(document.getElementById('agriculture').value)
     
+      //this adds all the stats
       const total = Economy + Military + Security + Welfare + Education + Agriculture;
       const result = document.getElementById('result');
 
+      //checks the stats are equal to 20
       if (total === 20) {
         alert("Success! The total is exactly 20.");
-
+        
+        //fetches the python post method returning the new stats in a json file
         fetch("http://127.0.0.1:8000/stats", {
         method: "POST",
+       //using json
         headers: {
             "Content-Type": "application/json",
         },
+        //this insertes the stats
         body: JSON.stringify({ economy : Economy , 
             military : Military ,
             security : Security ,
@@ -30,10 +36,12 @@ function checkSum() {
             console.log("Server response:", data);
             // You can update the UI or perform other actions with the received data here
         })
-
+        //opens the scenarion page is the stats equal 20
         window.location.href = "senario_page.html";
 
-      } else {
+      } 
+      //error message for if the su doesnt equal 20
+      else {
         result.textContent = `The sum is ${total}. Try again!`;
         result.style.color = '#FF3333';
       }
