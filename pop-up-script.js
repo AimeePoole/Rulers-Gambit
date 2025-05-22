b31c0cf729e89f03b015cefc8c5e932e3e42f67c
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-22 01:07:12 +0100

    comment fix

--- pop-up-script.js
+++ pop-up-script.js
@@ -1,15 +1,19 @@
+//get elements by id once and save them as consts
 const openPopup = document.getElementById('openPopupButton');
 const closePopup = document.getElementById('closePopupButton');
 const popup = document.getElementById('popup');
 
+//opens popup when button is clicked
 openPopup.addEventListener('click', () => {
     popup.style.display = 'flex';
 });
 
+//closes popup when button is clicked
 closePopup.addEventListener('click', () => {
     popup.style.display = 'none';
 });
 
+//closes popup when outside the window is clicked
 window.addEventListener('click', (event) => {
     if (event.target === popup) {
         popup.style.display = 'none';
