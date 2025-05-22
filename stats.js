e8c37375e8eee23ee1303c11b07ece66936660a9
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-22 12:46:47 +0100

    comment fix

--- stats.js
+++ stats.js
@@ -1,3 +1,4 @@
+//this gets the stats from html and will post them to the python
 function checkSum() {
   //this gets all the stat values entered as consts 
   const Economy = parseInt(document.getElementById('economy').value);
