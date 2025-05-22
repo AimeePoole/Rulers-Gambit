e11dcba38d4750fd7eea9e03b7b8d529d2637c89
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-22 01:00:18 +0100

    some syntax and comments were edited

--- scenario.js
+++ scenario.js
@@ -1,24 +1,33 @@
 
+//fetches the python get method getting a json file with the scenario description option decription and option mechanics
+
 fetch('http://127.0.0.1:8000/scenarioDetails')
   .then(function (response) {
     return response.json();
   }).then(function (data) {
     console.log(data);
+    //gets the correct html element
     document.getElementById("getScenario").innerHTML = data.scenarioDescription;
     // this might work 
     //document.getElementById("getScenario").innerHTML = data.id, data.optionDescription, data.scenario_id;
   });
 
 
+//fetches the python get method returning a json filw with the players stats 
+//put in a function so this can be refreshed every time a scenario is answered
 fetch('http://127.0.0.1:8000/playerStats')
   .then(function (response) {
     return response.json();
-  }).then(function (data) {
+  })
+  .then(function (data) {
     console.log(data);
-    let html = "";
+    let statsStyled = "";
+
+    // let each stat be output like "Economy: 3"
     data.forEach(item => {
-      html += item.statName + ": " + item.statsValue + "<br>";
+      statsStyled += item.statName + ": " + item.statsValue + "<br>";
     });
 
-    document.getElementById("getStats").innerHTML = html;
+    // gets the correct html element
+    document.getElementById("getStats").innerHTML = statsStyled;
   });
