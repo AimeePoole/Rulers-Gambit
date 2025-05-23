2296ff697c9f79cc548d2d205816f6aaa9eefe30
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-23 14:16:40 +0100

    scenario gen now get the id of the option picked

--- scenario.js
+++ scenario.js
@@ -1,11 +1,13 @@
 //inactive
 //fetches the python get method getting a json file with the scenario description option decription and option mechanics
+let scenarioData = null;
 
 fetch('http://127.0.0.1:8000/scenarioDetails')
   .then(function (response) {
     return response.json();
   }).then(function (data) {
     console.log(data);
+    scenarioData = data;
     //gets the correct html element
     document.getElementById("getScenario").innerHTML = data.scenarioDescription;
 
@@ -13,13 +15,27 @@ fetch('http://127.0.0.1:8000/scenarioDetails')
     document.getElementById("getOption2").innerHTML = data.options[1].optionDescription;
     document.getElementById("getOption3").innerHTML = data.options[2].optionDescription;
     document.getElementById("getOption4").innerHTML = data.options[3].optionDescription;
-
-
-    // this might work 
-    //document.getElementById("getScenario").innerHTML = data.id, data.optionDescription, data.scenario_id;
   });
 
 
+
+  function getOptionPickedId(data)
+  {
+    if (!scenarioData) {
+      document.getElementById("result").innerHTML = "Scenario data not loaded yet.";
+      return;
+    }
+
+    const selected = parseInt(document.querySelector('input[name="Options"]:checked').value);
+    document.getElementById("result").innerHTML = scenarioData.options[selected].option_id
+;
+  }
+  
+
+  
+
+  
+  
 //fetches the python get method returning a json filw with the players stats 
 //put in a function so this can be refreshed every time a scenario is answered
 fetch('http://127.0.0.1:8000/playerStats')
@@ -41,7 +57,4 @@ fetch('http://127.0.0.1:8000/playerStats')
 
 
 
-function displayRadioValue() {
-  const selected = document.querySelector('input[name="Options"]:checked').value;
-  document.getElementById("result").innerHTML = selected;
-}
+
