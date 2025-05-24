1c49611e85ac0c49391a87e0ca8bcb70e3d0d001
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-24 19:36:08 +0100

    code to get the final score through adding all the stats together

---
+++ endPage.js
@@ -0,0 +1,18 @@
+//fetches the python get method returning a json file with the players stats 
+//this gets the final score through adding all the stats together
+fetch('http://127.0.0.1:8000/playerStats')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    let total = 0;
+
+    // let each stat be output like "Economy: 3"
+    data.forEach(item => {
+      total += parseInt(item.statsValue)
+    });
+
+    // gets the correct html element
+    document.getElementById("endScore").innerHTML = total;
+  });
