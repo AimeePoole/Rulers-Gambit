20c29abdcee3bb2cbf86e1186b6c3dd7e0d4205e
e-perry24 <elizabeth.perry24@bathspa.ac.uk>
2025-05-25 02:18:57 +0100

    rough draft counter code

--- counter_code.js
+++ counter_code.js
@@ -5,10 +5,10 @@
 //https://www.tutorialspoint.com/how-to-count-the-number-of-times-a-button-is-clicked-using-javascript#:~:text=Tracking%20button%20clicks%20is%20a,keep%20track%20of%20button%20clicks.
 
 
-//var count = 0;
-//var button = document.getElementById("myButton");
-//var result = document.getElementById("result");
-//button.onclick = function() {
-//    count++;
-//    result.innerHTML = count;
-//}
+var count = 0;
+var button = document.getElementById("myButton");
+var result = document.getElementById("result");
+button.onclick = function() {
+    count++;
+    result.innerHTML = count;
+}
