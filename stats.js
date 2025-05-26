67076a9a383a3e3311a74c3c3b2d95ba4d38b829
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-26 16:19:05 +0100

    changed the total for stats to 30 as it was to easy to die in one turn

--- stats.js
+++ stats.js
@@ -13,9 +13,9 @@ function checkSum() {
   const total = Economy + Military + Security + Welfare + Education + Agriculture;
   const result = document.getElementById('result');
 
-  //checks the stats are equal to 20
-  if (total === 20) {
-    alert("Success! The total is exactly 20.");
+  //checks the stats are equal to 30
+  if (total === 30) {
+    alert("Success! The total is exactly 30.");
 
     //fetches the python post method returning the new stats in a json file
     fetch("http://127.0.0.1:8000/stats", {
@@ -39,11 +39,11 @@ function checkSum() {
         console.log("Server response:", data);
         // You can update the UI or perform other actions with the received data here
       })
-    //opens the scenarion page is the stats equal 20
+    //opens the scenarion page is the stats equal 30
     window.location.href = "senario_page.html";
 
   }
-  //error message for if the sum doesnt equal 20
+  //error message for if the sum doesnt equal 30
   else {
     result.textContent = `The sum is ${total}. Try again!`;
     result.style.color = '#FF3333';
