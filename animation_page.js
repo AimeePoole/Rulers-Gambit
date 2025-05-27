18b4798ba99ced5aa62e826b95cc3cf8e9fbb946
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-05-27 15:17:28 +0100

    getElementById

--- animation_page.js
+++ animation_page.js
@@ -1,11 +1,13 @@
 //active
-function animation_buttonid () {
-var spinner = animation_page.html("#news_paper");
-animation_page.html("#spin_button").onclick = function() {
-  spinner.style.animationName = "example";
-  setTimeout(function() {
-    spinner.style.animationName = "";
-  }, 4000);
-};
+function animation_buttonid() {
+  var spinner = document.getElementById('news_paper')
+  //animation_page.html("#news_paper");
+  //animation_page.html("#spin_button").
+  document.getElementById('spin_button').onclick = function () {
+    spinner.style.animationName = "example";
+    setTimeout(function () {
+      spinner.style.animationName = "";
+    }, 4000);
+  };
 }
 
