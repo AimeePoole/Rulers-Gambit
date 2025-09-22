edf703ee6cec2f8978e43c3c88b6983e900b6cf7
AimeePoole <AIMEE.POOLE24@bathspa.ac.uk>
2025-09-22 14:50:58 +0100

    small change

--- more_scenarios.py
+++ more_scenarios.py
@@ -10,7 +10,9 @@ cursor = connect.cursor()
 cursor.execute('INSERT INTO scenario \
     (id, scenarioDescription, catagory_id, phase_id) VALUES \
     ("s5", "A plage of incets threten to inialte your crops, jepoidsing your trade and agriculde", "c1", "p1"), \' \
-    ("s10", "You hear rumors of an assasination attempt on your life, what do you do?", "c2", "p1"), \
+    ("s10", "You hear rumors of an assasination attempt on your life, what do you do?", "c2", "p1")\
+    ')
+               
                
 
 
@@ -23,7 +25,8 @@ cursor.execute('INSERT INTO options \
     ("o101", "Run an investigation and start arresting people who are suspect ", "s10"), \
     ("o102", "Fortify your castle, barricade the walls to protect yourself ", "s10"), \
     ("o103", "let them try, no one would dear try and assassinate you ", "s10"), \
-    ("o104", "execute all your advisors just in case you get the right one ", "s10"), \
+    ("o104", "execute all your advisors just in case you get the right one ", "s10") \
+    ')
     
                
 
@@ -48,3 +51,4 @@ cursor.execute('INSERT INTO affects \
     ("4","+1","o103"), \
     ("3","+1","o104"), \
     ("4","-1","o104"), \
+    ')
